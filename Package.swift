f35459ec12f4b877aa69a5938e3ea3d96ca7de7f
MyTechBlitz3000 <JustinLeopardoFuji365@outlook.com>
2026-06-05 17:31:57 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,7 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import CompilerPluginSupport
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "mlx-swift-lm",
@@ -13,31 +12,18 @@ let package = Package(
         .visionOS(.v1),
     ],
     products: [
-        .library(
-            name: "MLXLLM",
-            targets: ["MLXLLM"]),
-        .library(
-            name: "MLXVLM",
-            targets: ["MLXVLM"]),
-        .library(
-            name: "MLXLMCommon",
-            targets: ["MLXLMCommon"]),
-        .library(
-            name: "MLXEmbedders",
-            targets: ["MLXEmbedders"]),
-        .library(
-            name: "MLXHuggingFace",
-            targets: ["MLXHuggingFace"]),
-        .library(
-            name: "BenchmarkHelpers",
-            targets: ["BenchmarkHelpers"]),
-        .library(
-            name: "IntegrationTestHelpers",
-            targets: ["IntegrationTestHelpers"]),
+        .library(name: "MLXLLM", targets: ["MLXLLM"]),
+        .library(name: "MLXVLM", targets: ["MLXVLM"]),
+        .library(name: "MLXLMCommon", targets: ["MLXLMCommon"]),
+        .library(name: "MLXEmbedders", targets: ["MLXEmbedders"]),
+        .library(name: "MLXHuggingFace", targets: ["MLXHuggingFace"]),
+        .library(name: "BenchmarkHelpers", targets: ["BenchmarkHelpers"]),
+        .library(name: "IntegrationTestHelpers", targets: ["IntegrationTestHelpers"]),
     ],
     dependencies: [
         .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.31.4")),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "604.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
     ],
     targets: [
         .target(
@@ -49,10 +35,9 @@ let package = Package(
                 .product(name: "MLXOptimizers", package: "mlx-swift"),
             ],
             path: "Libraries/MLXLLM",
-            exclude: [
-                "README.md"
-            ]
+            exclude: ["README.md"]
         ),
+
         .target(
             name: "MLXVLM",
             dependencies: [
@@ -62,10 +47,9 @@ let package = Package(
                 .product(name: "MLXOptimizers", package: "mlx-swift"),
             ],
             path: "Libraries/MLXVLM",
-            exclude: [
-                "README.md"
-            ]
+            exclude: ["README.md"]
         ),
+
         .target(
             name: "MLXLMCommon",
             dependencies: [
@@ -74,10 +58,9 @@ let package = Package(
                 .product(name: "MLXOptimizers", package: "mlx-swift"),
             ],
             path: "Libraries/MLXLMCommon",
-            exclude: [
-                "README.md"
-            ]
+            exclude: ["README.md"]
         ),
+
         .target(
             name: "MLXEmbedders",
             dependencies: [
@@ -86,10 +69,9 @@ let package = Package(
                 .target(name: "MLXLMCommon"),
             ],
             path: "Libraries/MLXEmbedders",
-            exclude: [
-                "README.md"
-            ]
+            exclude: ["README.md"]
         ),
+
         .target(
             name: "BenchmarkHelpers",
             dependencies: [
@@ -101,6 +83,7 @@ let package = Package(
             ],
             path: "Libraries/BenchmarkHelpers"
         ),
+
         .target(
             name: "IntegrationTestHelpers",
             dependencies: [
@@ -113,6 +96,7 @@ let package = Package(
             path: "Libraries/IntegrationTestHelpers",
             exclude: ["README.md"]
         ),
+
         .testTarget(
             name: "MLXLMTests",
             dependencies: [
@@ -125,34 +109,19 @@ let package = Package(
                 "MLXEmbedders",
             ],
             path: "Tests/MLXLMTests",
-            exclude: [
-                "README.md"
-            ],
-            resources: [.process("Resources/1080p_30.mov"), .process("Resources/audio_only.mov")]
+            exclude: ["README.md"],
+            resources: [
+                .process("Resources/1080p_30.mov"),
+                .process("Resources/audio_only.mov")
+            ]
         ),
-        .macro(
-            name: "MLXHuggingFaceMacros",
-            dependencies: [
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
-            ],
-            path: "Libraries/MLXHuggingFaceMacros"
-        ),
+
         .target(
             name: "MLXHuggingFace",
             dependencies: [
-                "MLXHuggingFaceMacros",
                 "MLXLMCommon",
             ],
             path: "Libraries/MLXHuggingFace"
         ),
     ]
 )
-
-if Context.environment["MLX_SWIFT_BUILD_DOC"] == "1"
-    || Context.environment["SPI_GENERATE_DOCS"] == "1"
-{
-    package.dependencies.append(
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
-    )
-}
